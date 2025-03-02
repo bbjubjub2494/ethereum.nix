6943c0b0478e2060c07d23baec10e1bb38eda12c
Julie B. <no-reply@bbjubjub.fr>
2025-03-02 20:15:20 +0100

    nimbus: 5.2.0 -> 5.3.0

--- pkgs/nimbus/default.nix
+++ pkgs/nimbus/default.nix
@@ -4,12 +4,12 @@
   targets ? ["nimbus_beacon_node" "nimbus_validator_client" "gnosis-build" "gnosis-vc-build"],
   stableSystems ? ["x86_64-linux" "aarch64-linux"],
 }: let
-  version = "25.2.0";
+  version = "25.3.0";
   src = fetchFromGitHub {
     owner = "status-im";
     repo = "nimbus-eth2";
     rev = "v${version}";
-    hash = "sha256-INALxgkuZ4kT/DrpzMBqgNeM8Ar78BRccmGWtqoGhB0=";
+    hash = "sha256-v81pFj8V9IczTqbl3l3+NEMfesdmdnV3kQEi6iYBES0=";
     fetchSubmodules = true;
   };
 in
