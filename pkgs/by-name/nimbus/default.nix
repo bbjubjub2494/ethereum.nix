27bec229d541a098a2becf2085d1927ab1f0a424
Julie B. <no-reply@bbjubjub.fr>
2025-09-27 14:30:52 +0200

    nimbus: 25.9.0 -> 25.9.2

--- pkgs/by-name/nimbus/default.nix
+++ pkgs/by-name/nimbus/default.nix
@@ -4,12 +4,12 @@
   targets ? ["nimbus_beacon_node" "nimbus_validator_client" "gnosis-build" "gnosis-vc-build"],
   stableSystems ? ["x86_64-linux" "aarch64-linux"],
 }: let
-  version = "25.9.0";
+  version = "25.9.2";
   src = fetchFromGitHub {
     owner = "status-im";
     repo = "nimbus-eth2";
     rev = "v${version}";
-    hash = "sha256-DHUPqYMv5navs4I0d4BaQ5Z4hDct8Y07UYekTMm4INw=";
+    hash = "sha256-TriDSV36oUd11m8mS6XruGXaoZuP4hx8a+Csp/qxKRw=";
     fetchSubmodules = true;
   };
 in
